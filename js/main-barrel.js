b18527201ce8ab6f3bbdf26cc857faf4d4f0d2e4
Carlos E Alford <webshuriken@gmail.com>
2026-03-27 17:54:20 +0000

    refactor: move the levels folder within the js folder

--- js/main-barrel.js
+++ js/main-barrel.js
@@ -1,6 +1,6 @@
-import { levelBlueprint as demo } from '../levels/demo.js';
-import { levelBlueprint as leve2 } from '../levels/level2.js';
-import { levelBlueprint as leve3 } from '../levels/level3.js';
+import { levelBlueprint as demo } from './levels/demo.js';
+import { levelBlueprint as leve2 } from './levels/level2.js';
+import { levelBlueprint as leve3 } from './levels/level3.js';
 
 // consumed by the game engine
 const GAME_LEVELS = [

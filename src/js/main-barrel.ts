fdfd059b39387bfecd30c8895d422f2aeb57ee05
Carlos E Alford <webshuriken@gmail.com>
2026-04-05 14:58:24 +0100

    feat: add types to main-barrel.ts file

--- src/js/main-barrel.ts
+++ src/js/main-barrel.ts
@@ -1,9 +1,9 @@
-import { levelBlueprint as demo } from './levels/demo.js';
-import { levelBlueprint as leve2 } from './levels/level2.js';
-import { levelBlueprint as leve3 } from './levels/level3.js';
+import { levelBlueprint as demo } from './levels/demo';
+import { levelBlueprint as leve2 } from './levels/level2';
+import { levelBlueprint as leve3 } from './levels/level3';
 
 // consumed by the game engine
-const GAME_LEVELS = [
+const GAME_LEVELS: string[] = [
   demo.map,
   leve2.map,
   leve3.map

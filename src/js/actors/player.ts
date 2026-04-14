2566c731f02c0951eeac2748701077de31abf8c1
Carlos E Alford <webshuriken@gmail.com>
2026-04-14 23:13:09 +0100

    feat: migrate Player class to TS

--- src/js/actors/player.ts
+++ src/js/actors/player.ts
@@ -1,12 +1,22 @@
 /**
  * Jumping Monolith - Player Class
  * created: 2026
- * updated: 2026
+ * updated: 05-04-2026
  * author: Carlos E Alford
  * improvements:
  */
 
-import { Vector } from "../utilities/vector.js";
+import { Vector, type IVector } from "../utilities/vector";
+import { type IState, type TTrackKeys } from "../utilities/state";
+
+
+export interface IPlayer {
+  pos: IVector;
+  readonly size: IVector;
+  readonly type: 'player';
+  speed: IVector;
+  update(time: number, state: IState, keys: TTrackKeys): Player;
+}
 
 // player abilities
 const playerXSpeed = 7;
@@ -15,67 +25,63 @@ const jumpSpeed = 17;
 
 /**
  * @description Create a player actor
- * @param {object} pos - actors current location
- * @param {integer} speed - actors current speed
+ * @param {IVector} pos - actors current location
+ * @param {IVector} speed - actors current speed
  */
-class Player {
-  constructor(pos, speed) {
-    this.pos = pos;
-    // Stores current speed to simulate momentum and gravity.
-    this.speed = speed;
-  }
+class Player implements IPlayer {
+  // Size is the same for all instances, add to prototype
+  readonly size = new Vector(0.8, 1.5);
 
-  get type() { return "player"; }
+  // speed simulates momentum and gravity.
+  constructor(public pos: IVector, public speed: IVector) {}
+
+  get type(): 'player' { return "player"; }
 
   // A player is one-and-a-half squares high, initial position is set to
   // be half a square above the position where the @ character appeared.
-  static create(pos) {
+  static create(pos: IVector) {
     return new Player(pos.plus(new Vector(0, -0.5)), new Vector(0, 0));
   }
+
+  /**
+   * Hanlde player motion inside of map. Motion is handled per axis.
+   * @param {number} time - time step
+   * @param {IState} state - current game state
+   * @param {TTrackKeys} keys - game keys
+   * @return {Player}
+   */
+  update(time: number, state: IState, keys: TTrackKeys): Player {
+    // Calculate left and right movement
+    let xSpeed = 0;
+    if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+    if (keys.ArrowRight) xSpeed += playerXSpeed;
+
+    // With no wall blocking new position is used
+    let pos = this.pos;
+    let movedX = pos.plus(new Vector(xSpeed * time, 0));
+
+    if (!state.level.touches(movedX, this.size, "wall")) {
+      pos = movedX;
+    }
+
+    // Calculate up and down movement, using gravity and jumping.
+
+    // ySPeed accelerated to account for gravity
+    let ySpeed = this.speed.y + time * gravity;
+    let movedY = pos.plus(new Vector(0, ySpeed * time));
+
+    // Check for walls before making a move, out a possible two:
+    // 1. arrow key up pressed and moving down, speed is changed to jump again.
+    // 2. player simply bumped into something and speed is set to zero.
+    if (!state.level.touches(movedY, this.size, "wall")) {
+      pos = movedY;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+      ySpeed = -jumpSpeed;
+    } else {
+      ySpeed = 0;
+    }
+    return new Player(pos, new Vector(xSpeed, ySpeed));
+  };
 }
-
-// Size is the same for all instances, add to prototype
-Player.prototype.size = new Vector(0.8, 1.5);
-
-/**
- * @description Hanlde player motion inside of map
- * @param {integer} time - time step
- * @param {object} state - current game state
- * @param {object} keys - game keys
- * @return {object}
- */
-Player.prototype.update = function(time, state, keys) {
-  // Player motion is handled per axis.
-
-  // Calculate left and right movement
-  let xSpeed = 0;
-  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
-  if (keys.ArrowRight) xSpeed += playerXSpeed;
-
-  // With no wall blocking new position is used
-  let pos = this.pos;
-  let movedX = pos.plus(new Vector(xSpeed * time, 0));
-  if (!state.level.touches(movedX, this.size, "wall")) {
-    pos = movedX;
-  }
-
-  // Calculate up and down movement, using gravity and jumping.
-
-  // ySPeed accelerated to account for gravity
-  let ySpeed = this.speed.y + time * gravity;
-  let movedY = pos.plus(new Vector(0, ySpeed * time));
-
-  // Check for walls before making a move, out a possible two:
-  // 1. arrow key up pressed and moving down, speed is changed to jump again.
-  // 2. player simply bumped into something and speed is set to zero.
-  if (!state.level.touches(movedY, this.size, "wall")) {
-    pos = movedY;
-  } else if (keys.ArrowUp && ySpeed > 0) {
-    ySpeed = -jumpSpeed;
-  } else {
-    ySpeed = 0;
-  }
-  return new Player(pos, new Vector(xSpeed, ySpeed));
-};
 
 export { Player };

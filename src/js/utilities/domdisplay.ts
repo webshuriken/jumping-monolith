37f9d4d4f2f1dacb2da423e65aad80d1caddd270
Carlos E Alford <webshuriken@gmail.com>
2026-04-14 23:14:11 +0100

    feat: migrate DOMDisplay class to TS

--- src/js/utilities/domdisplay.ts
+++ src/js/utilities/domdisplay.ts
@@ -1,7 +1,7 @@
 /**
  * Jumping Monolith - Game Utilities
  * created: 2026
- * updated: 2026
+ * updated: 05-04-2026
  * author: Carlos E Alford
  * utility: DOMDisplay, to maintain the browsers DOM
  * improvements:
@@ -12,7 +12,18 @@ import {
   drawGrid,
   elt,
   scale
- } from './util-functions.js';
+} from './util-functions';
+import { type ILevel } from './level';
+import { type IState } from './state';
+
+
+interface IDOMDisplay {
+  actorLayer: HTMLDivElement | null;
+  readonly dom: HTMLDivElement;
+  clear: () => {};
+  syncState(state: IState):  void;
+  scrollPlayerIntoView(state: IState): void;
+}
 
 /**
  * @description Displays a given level and state.
@@ -20,59 +31,68 @@ import {
  * @param {object} level - current level map
  */
 class DOMDisplay {
-  constructor(parent, level) {
+  readonly dom: HTMLDivElement;
+  actorLayer: HTMLDivElement | null;
+
+  constructor(parent: HTMLDivElement, level: ILevel) {
     this.dom = elt("div", {class: "game"}, drawGrid(level));
-    this.actorLayer = null;
     parent.appendChild(this.dom);
+    this.actorLayer = null;
   }
 
   clear() { this.dom.remove(); }
+
+  /**
+   * Make the display show a given Actor state.
+   * @param {IState} state - current game state
+   */
+  syncState(state: IState): void {
+    // remove old actors layer if one exists
+    if (this.actorLayer) this.actorLayer.remove();
+
+    // new actors layer
+    const layer = drawActors(state.actors);
+    this.actorLayer = layer;
+    this.dom.appendChild(layer);
+
+    // status added to element class to style game accordingly
+    this.dom.className = `game ${state.status}`;
+
+    // make sure player does not go off screen
+    this.scrollPlayerIntoView(state);
+  };
+
+  /**
+   * Keep the player within the center of the screen or there abouts
+   * @param {IState} state - current game state
+   */
+  scrollPlayerIntoView(state: IState): void {
+    // We change the scroll position by manipulating that element’s
+    // scrollLeft and scrollTop properties when the player is too
+    // close to the edge.
+    let width = this.dom.clientWidth;
+    let height = this.dom.clientHeight;
+    let margin = width / 3;
+
+    // The viewport
+    let left = this.dom.scrollLeft, right = left + width;
+    let top = this.dom.scrollTop, bottom = top + height;
+
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+    if (center.x < left + margin) {
+      this.dom.scrollLeft = center.x - margin;
+    } else if (center.x > right - margin) {
+      this.dom.scrollLeft = center.x + margin - width;
+    }
+
+    if (center.y < top + margin) {
+      this.dom.scrollTop = center.y - margin;
+    } else if (center.y > bottom - margin) {
+      this.dom.scrollTop = center.y + margin - height;
+    }
+  };
 }
-
-/**
- * @description Make the display show a given Actor state.
- * @param {string} state - current game state
- */
-DOMDisplay.prototype.syncState = function(state) {
-  // remove old actors layer if one exists
-  if (this.actorLayer) this.actorLayer.remove();
-  // Update the actors
-  this.actorLayer = drawActors(state.actors);
-  this.dom.appendChild(this.actorLayer);
-  this.dom.className = `game ${state.status}`;
-  this.scrollPlayerIntoView(state);
-};
-
-/**
- * @description find the player’s position and update the wrapping element’s scroll position.
- * @param {object} state - current game state
- */
-DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
-  // We change the scroll position by manipulating that element’s
-  // scrollLeft and scrollTop properties when the player is too
-  // close to the edge.
-  let width = this.dom.clientWidth;
-  let height = this.dom.clientHeight;
-  let margin = width / 3;
-
-  // The viewport
-  let left = this.dom.scrollLeft, right = left + width;
-  let top = this.dom.scrollTop, bottom = top + height;
-
-  let player = state.player;
-  let center = player.pos.plus(player.size.times(0.5)).times(scale);
-
-  if (center.x < left + margin) {
-    this.dom.scrollLeft = center.x - margin;
-  } else if (center.x > right - margin) {
-    this.dom.scrollLeft = center.x + margin - width;
-  }
-
-  if (center.y < top + margin) {
-    this.dom.scrollTop = center.y - margin;
-  } else if (center.y > bottom - margin) {
-    this.dom.scrollTop = center.y + margin - height;
-  }
-};
 
 export { DOMDisplay };

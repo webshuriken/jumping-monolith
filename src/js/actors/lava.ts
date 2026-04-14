e08166547a1154ef7eb22859b59c77959c164405
Carlos E Alford <webshuriken@gmail.com>
2026-04-14 23:13:18 +0100

    feat: migrate Lava class to TS

--- src/js/actors/lava.ts
+++ src/js/actors/lava.ts
@@ -1,71 +1,88 @@
 /**
  * Jumping Monolith - Lava Class
  * created: 2026
- * updated: 2026
+ * updated: 05-04-2026
  * author: Carlos E Alford
  * improvements:
  */
 
-import { Vector } from "../utilities/vector.js";
-import { State } from "../utilities/state.js";
+import { Vector, type IVector } from "../utilities/vector";
+import { State, type IState } from "../utilities/state";
 
-/**
- * @description Create a lava type actor
- * @param {object} pos - actors current location
- * @param {integer} speed - actors current speed
- */
-class Lava {
-  constructor(pos, speed, reset) {
-    this.pos = pos;
-    this.speed = speed;
-    this.reset = reset;
-  }
 
-  get type() { return "lava"; }
+export type TLavaChars = "=" | "|" | "v";
 
-  // actor character type required to choose lava
-  static create(pos, ch) {
-    // Decide which type of lava to build
-    if (ch == "=") {
-      return new Lava(pos, new Vector(2, 0));
-    } else if (ch == "|") {
-      return new Lava(pos, new Vector(0, 2));
-    } else if (ch == "v") {
-      return new Lava(pos, new Vector(0, 3), pos);
-    }
-  }
+export interface ILava {
+  collide(state: IState): IState;
+  pos: IVector;
+  readonly type: 'lava';
+  reset?: IVector;
+  size: IVector;
+  speed: IVector;
+  update(time: number, state: IState): Lava;
 }
 
-Lava.prototype.size = new Vector(1, 1);
-
 /**
- * @description Updates the Player state on collide with Lava
- * @param {object} state - current game state
- * @return {object}
+ * Create a lava type actor
+ * @param {IVector} pos actors current location
+ * @param {IVector} speed actors current speed
+ * @param {IVector} reset?
  */
-Lava.prototype.collide = function(state) {
-  return new State(state.level, state.actors, "lost");
-};
+class Lava implements ILava {
+  // all lava is same size
+  readonly size = new Vector(1, 1);
+  reset?: IVector;
 
-/**
- * @description Computes a new position for Lava actor
- * @param {integer} time - time step
- * @param {object} state - current game state
- * @return {object}
- */
-Lava.prototype.update = function(time, state) {
-  // Add the product of the time step and the current speed to its old position
-  let newPos = this.pos.plus(this.speed.times(time));
-  // The bahaviour depends on the type of lava block
-  if (!state.level.touches(newPos, this.size, "wall")) {
-    return new Lava(newPos, this.speed, this.reset);
-  } else if (this.reset) {
-    // Dripping lava resets its position
-    return new Lava(this.reset, this.speed, this.reset);
-  } else {
-    // Bouncing lava inverts its speed
-    return new Lava(this.pos, this.speed.times(-1));
+  constructor(public pos: IVector, public speed: IVector, reset?: IVector) {
+    if (reset) {
+      this.reset = reset;
+    }
   }
-};
+
+  get type(): 'lava' { return "lava"; }
+
+  // actor character type required to choose lava
+  static create(pos: IVector, ch: TLavaChars): Lava {
+    // Decide which type of lava to build
+    switch (ch) {
+      case "=":
+        return new Lava(pos, new Vector(2, 0));
+      case "|":
+        return new Lava(pos, new Vector(0, 2));
+      case "v":
+        return new Lava(pos, new Vector(0, 3), pos);
+    }
+  }
+
+  /**
+   * Updates the Player state on collide with Lava
+   * @param {IState} state - current game state
+   * @return {IState}
+   */
+  collide(state: IState): State {
+    return new State(state.level, state.actors, "lost");
+  }
+
+  /**
+   * Computes a new position for Lava actor
+   * @param {number} time - time step
+   * @param {IState} state - current game state
+   * @return {Lava}
+   */
+  update(time: number, state: IState): Lava {
+    // Add the product of the time step and the current speed to its old position
+    let newPos = this.pos.plus(this.speed.times(time));
+    // The bahaviour depends on the type of lava block
+    if (!state.level.touches(newPos, this.size, "wall")) {
+      return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+      // Dripping lava resets its position
+      return new Lava(this.reset, this.speed, this.reset);
+    } else {
+      // Bouncing lava inverts its speed
+      return new Lava(this.pos, this.speed.times(-1));
+    }
+  };
+}
 
 export { Lava };

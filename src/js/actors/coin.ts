403271a61c1f3a852dd68a530d73d6afd011a180
Carlos E Alford <webshuriken@gmail.com>
2026-04-14 23:12:47 +0100

    feat: migrate Coin class to TS

--- src/js/actors/coin.ts
+++ src/js/actors/coin.ts
@@ -1,75 +1,79 @@
 /**
  * Jumping Monolith - Coin Class
  * created: 2026
- * updated: 2026
+ * updated: 05-04-2026
  * author: Carlos E Alford
  * improvements:
  */
 
-import { Vector } from "../utilities/vector.js";
-import { State } from "../utilities/state.js";
+import { Vector, type IVector } from "../utilities/vector";
+import { State, type IState } from "../utilities/state";
+
+interface ICoin {
+  basePos:IVector;
+  collide(state: IState): IState;
+  pos: IVector;
+  readonly size: IVector;
+  readonly type: 'coin';
+  update(time: number): Coin;
+  wobble: number;
+}
 
 // how fast the coins move and how far
 const wobbleSpeed = 8;
 const wobbleDist = 0.07;
 
 /**
- * @description Create a coin actor
- * @param {object} pos - actors current location
- * @param {object} basePos - base position
- * @param {object} wobble - the wobble
+ * Create a coin actor
+ * @param {IVector} pos - actors current location
+ * @param {IVector} basePos - base position
+ * @param {number} wobble - the wobble
  */
-class Coin {
+class Coin implements ICoin {
+  // all coins are the same size
+  readonly size: IVector = new Vector(0.6, 0.6);
+ 
   // are given a “wobble”, a slight vertical back-and-forth motion.
   // basePos and wobble determine the coins current pos
-  constructor(pos, basePos, wobble) {
-    this.pos = pos;
-    this.basePos = basePos;
-    this.wobble = wobble;
-  }
+  constructor(public pos: IVector, public basePos: IVector, public wobble: number) {}
 
-  get type() { return "coin"; }
+  get type(): 'coin' { return "coin"; }
 
-  static create(pos) {
+  static create(pos: IVector): Coin {
     let basePos = pos.plus(new Vector(0.2, 0.1));
     // starting pos is randomised to avoid all coins moving in sync
     return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
   }
+
+  /**
+ * Updates the Player state on collide with Coin
+ * @param {IState} state - current game state
+ * @return {State}
+ */
+  collide(state: IState): State {
+    // remove the collected coin if any
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    // check for leftover coins
+    if (!filtered.some(a => a.type == "coin")) status = "won";
+    return new State(state.level, filtered, status);
+  };
+
+  /**
+   * Create the coins wobble
+   * @param {number} time - time step
+   * @return {Coin}
+   */
+  update(time: number): Coin {
+    // Coins use their update method to wobble. They ignore collisions with the
+    // grid since they are simply wobbling around inside of their own square.
+
+    // property used to track time and used as argument to Math.sin
+    let wobble = this.wobble + time * wobbleSpeed;
+    // coins new position computed from its base pos and an offset based on this wave
+    let wobblePos = Math.sin(wobble) * wobbleDist;
+    return new Coin(this.basePos.plus(new Vector(0, wobblePos)), this.basePos, wobble);
+  };
 }
-
-// All coins are same size so place in prototype
-Coin.prototype.size = new Vector(0.6, 0.6);
-
-/**
- * @description Updates the Player state on collide with Coin
- * @param {object} state - current game state
- * @return {object}
- */
-Coin.prototype.collide = function(state) {
-  // remove the collected coin if any
-  let filtered = state.actors.filter(a => a != this);
-  let status = state.status;
-  // check for leftover coins
-  if (!filtered.some(a => a.type == "coin")) status = "won";
-  return new State(state.level, filtered, status);
-};
-
-// Coins use their update method to wobble. They ignore collisions with the
-// grid since they are simply wobbling around inside of their own square.
-/**
- * @description Create the coins wobble
- * @param {integer} time - time step
- * @return {object}
- */
-Coin.prototype.update = function(time) {
-  // method ignores collisions with the grid since they are simply
-  // wobbling around in their own square.
-
-  // property used to track time and used as argument to Math.sin
-  let wobble = this.wobble + time * wobbleSpeed;
-  // coins new position computed from its base pos and an offset based on this wave
-  let wobblePos = Math.sin(wobble) * wobbleDist;
-  return new Coin(this.basePos.plus(new Vector(0, wobblePos)), this.basePos, wobble);
-};
 
 export { Coin };
